616c7d020d8b80c4ab1b7df1f3b1cb36c8d9deb7
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-03-18 08:34:14 -0500

    added work

--- src/main/java/com/codeup/springblog/Controllers/PostController.java
+++ src/main/java/com/codeup/springblog/Controllers/PostController.java
@@ -1,19 +1,29 @@
-package com.codeup.springblog;
+package com.codeup.springblog.Controllers;
 
+import com.codeup.springblog.Models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String getPost(){
-        return "posts index page";
+    public String getPosts(Model model){
+        ArrayList<Post> postList = new ArrayList<>();
+        postList.add(new Post((long) 2, "Second Post", "askdfhkashdfkjahsdf"));
+        postList.add(new Post((long) 3, "Third Post", "some more text..."));
+
+        model.addAttribute("posts", postList);
+        return "posts/index";
     }
+
+
     @GetMapping("/posts/{id}")
     @ResponseBody
     public String getPost(@PathVariable int id){
